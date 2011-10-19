a4c258857c96a2d83101893432a4b4f65884b382
Robin Clarke <rcl@cpan.org>
2011-10-19 11:52:42 +0000

    Import of RCL/Daemon-Shutdown-0.07 from CPAN.
    gitpan-cpan-distribution: Daemon-Shutdown
    gitpan-cpan-version:      0.07
    gitpan-cpan-path:         RCL/Daemon-Shutdown-0.07.tar.gz
    gitpan-cpan-author:       RCL
    gitpan-cpan-maturity:     released

--- lib/Daemon/Shutdown.pm
+++ lib/Daemon/Shutdown.pm
@@ -8,6 +8,7 @@ use Params::Validate qw/:all/;
 use File::Basename;
 use IPC::Run;
 use User;
+use AnyEvent;
 
 =head1 NAME
 
@@ -15,12 +16,11 @@ Daemon::Shutdown - A Shutdown Daemon
 
 =head1 VERSION
 
-Version 0.06
+Version 0.07
 
 =cut
 
-our $VERSION = '0.06';
-
+our $VERSION = '0.07';
 
 =head1 SYNOPSIS
 
@@ -97,7 +97,20 @@ Default: 0
 
 The full path to the shutdown binary
 
-Default: /sbin/shutdown
+Default: /sbin/poweroff
+
+=item shutdown_args <ArrayRef>
+
+Any args to pass to your shutdown_binary
+
+Default: none
+
+=item shutdown_after_triggered_monitors <Str>
+
+The number of monitors which need to be triggered at the same time to cause a 
+shutdown. Can be a number or the word 'all'.
+
+Default: 1
 
 =back
 
@@ -107,6 +120,9 @@ Default: /sbin/shutdown
 log_level: INFO
 log_file: /var/log/sdd.log
 shutdown_binary: /sbin/shutdown
+shutdown_args:
+  - -h
+  - now
 exit_after_trigger: 0
 sleep_before_run: 30
 verbose: 0
@@ -123,30 +139,32 @@ monitor:
 
 sub new {
     my $class = shift;
-   
+
     my %params = @_;
+
     # Remove any undefined parameters from the params hash
-    map{ delete( $params{$_} ) if not $params{$_} } keys %params;
-    
+    map { delete( $params{$_} ) if not $params{$_} } keys %params;
+
     # Validate the config file
     %params = validate_with(
         params => \%params,
         spec   => {
-            config   => {
-                callbacks => { 'File exists' => sub{ -f shift } },
-                default   => '/etc/sdd.conf',
+            config => {
+                callbacks => {
+                    'File exists' => sub { -f shift }
+                },
+                default => '/etc/sdd.conf',
             },
         },
         allow_extra => 1,
     );
     my $self = {};
-        
 
     # Read the config file
-    if( not $params{config} ){
+    if ( not $params{config} ) {
         $params{config} = '/etc/sdd.conf';
     }
-    if( not -f $params{config} ){
+    if ( not -f $params{config} ) {
         die( "Config file $params{config} not found\n" );
     }
     my $file_config = LoadFile( $params{config} );
@@ -155,64 +173,72 @@ sub new {
     # Merge the default, config file, and passed parameters
     %params = ( %$file_config, %params );
 
-    my @validate = map{ $_, $params{$_} } keys( %params );
+    my @validate = map { $_, $params{$_} } keys( %params );
     %params = validate_with(
         params => \%params,
-        spec   => 
-            {
-                log_file => {
-                    default   => '/var/log/sdd.log',
-                    callbacks => {
-                        'Log file is writable' => sub{ 
-                            my $filepath = shift;
-                            if( -f $filepath ) {
-                                return -w $filepath;
-                            }
-                            else
-                            {
-                                # Is directory writable
-                                return -w dirname( $filepath );
-                            }
-                        },
+        spec   => {
+            log_file => {
+                default   => '/var/log/sdd.log',
+                callbacks => {
+                    'Log file is writable' => sub {
+                        my $filepath = shift;
+                        if ( -f $filepath ) {
+                            return -w $filepath;
+                        } else {
+
+                            # Is directory writable
+                            return -w dirname( $filepath );
+                        }
                     },
-                },
-                log_level => {
-                    default   => 'INFO',
-                    regex     => qr/^(DEBUG|INFO|WARN|ERROR)$/,
-                },
-                verbose => {
-                    default => 0,
-                    regex   => qr/^[1|0]$/,
-                },
-                test => {
-                    default => 0,
-                    regex   => qr/^[1|0]$/,
-                },
-                sleep_before_run => {
-                    default => 3600,
-                    regex   => qr/^\d*$/,
-                },
-                exit_after_trigger => {
-                    default => 0,
-                    regex   => qr/^[1|0]$/,
-                },
-                use_sudo => {
-                    default => 0,
-                    regex   => qr/^[1|0]$/,
-                },
-                shutdown_binary => {
-                    default => '/sbin/shutdown',
-                    type => 'SCALAR',
-                    callbacks => {
-                        'Shutdown binary exists' => sub{ -x shift() },
-                    },
-                }, 
-                monitor => {
-                    type  => HASHREF,
                 },
             },
+            log_level => {
+                default => 'INFO',
+                regex   => qr/^(DEBUG|INFO|WARN|ERROR)$/,
+            },
+            verbose => {
+                default => 0,
+                regex   => qr/^[1|0]$/,
+            },
+            test => {
+                default => 0,
+                regex   => qr/^[1|0]$/,
+            },
+            sleep_before_run => {
+                default => 3600,
+                regex   => qr/^\d*$/,
+            },
+            exit_after_trigger => {
+                default => 0,
+                regex   => qr/^[1|0]$/,
+            },
+            use_sudo => {
+                default => 0,
+                regex   => qr/^[1|0]$/,
+            },
+            shutdown_binary => {
+                default   => '/sbin/poweroff',
+                type      => SCALAR,
+                callbacks => {
+                    'Shutdown binary exists' => sub {
+                        -x shift();
+                    },
+                },
+            },
+            shutdown_args => {
+                type     => ARRAYREF,
+                optional => 1
+            },
+            monitor                           => { type => HASHREF, },
+            shutdown_after_triggered_monitors => {
+                default => 1,
+                type    => SCALAR,
+                regex   => qr/^(all|\d+)$/,
+            }
+        },
+
         # A little less verbose than Carp...
-        on_fail => sub{ die( shift() ) },
+        on_fail => sub { die( shift() ) },
     );
 
     $self->{params} = \%params;
@@ -220,8 +246,8 @@ sub new {
     bless $self, $class;
 
     # Set up the logging
-    my  $log4perl_conf = sprintf 'log4perl.rootLogger = %s, Logfile', $params{log_level} || 'WARN';
-    if( $params{verbose} > 0 ){
+    my $log4perl_conf = sprintf 'log4perl.rootLogger = %s, Logfile', $params{log_level} || 'WARN';
+    if ( $params{verbose} > 0 ) {
         $log4perl_conf .= q(, Screen
             log4perl.appender.Screen         = Log::Log4perl::Appender::Screen
             log4perl.appender.Screen.stderr  = 0
@@ -242,32 +268,120 @@ sub new {
     Log::Log4perl::init( \$log4perl_conf );
     my $logger = Log::Log4perl->get_logger();
     $self->{logger} = $logger;
-    
 
     $self->{is_root} = ( User->Login eq 'root' ? 1 : 0 );
     $self->{logger}->info( "You are " . User->Login );
 
-    if( not $self->{is_root} ){
-	$self->{logger}->warn( "You are not root. SDD will probably not work..." );
+    if ( not $self->{is_root} ) {
+        $self->{logger}->warn( "You are not root. SDD will probably not work..." );
     }
 
     # Load the monitors
     my %monitors;
-    foreach my $monitor_name( keys( %{ $params{monitor} } ) ){
-	eval{
-	    my $monitor_package = 'Daemon::Shutdown::Monitor::' . $monitor_name;
-	    my $monitor_path = 'Daemon/Shutdown/Monitor/' . $monitor_name . '.pm';
-	    require $monitor_path;
+    foreach my $monitor_name ( keys( %{ $params{monitor} } ) ) {
+        eval {
+            my $monitor_package = 'Daemon::Shutdown::Monitor::' . $monitor_name;
+            my $monitor_path    = 'Daemon/Shutdown/Monitor/' . $monitor_name . '.pm';
+            require $monitor_path;
 
-	    $monitors{$monitor_name} = $monitor_package->new( %{ $params{monitor}->{$monitor_name} } );
-	};
-	if( $@ ){
-	    die( "Could not initialise monitor: $monitor_name\n$@\n" );
-	}
+            $monitors{$monitor_name} = $monitor_package->new( %{ $params{monitor}->{$monitor_name} } );
+        };
+        if ( $@ ) {
+            die( "Could not initialise monitor: $monitor_name\n$@\n" );
+        }
     }
-    $self->{monitors} = \%monitors;
+    $self->{monitors}           = \%monitors;
+    $self->{triggered_monitors} = {};
 
+    my $num_monitors = keys %monitors;
+    if (   $self->{params}->{shutdown_after_triggered_monitors} eq 'all'
+        || $self->{params}->{shutdown_after_triggered_monitors} > $num_monitors )
+    {
+        $self->{params}->{shutdown_after_triggered_monitors} = $num_monitors;
+    }
+
+    $logger->debug(
+        sprintf "Will shutdown if %d of %d monitors agree",
+        $self->{params}->{shutdown_after_triggered_monitors},
+        $num_monitors
+    );
+    $self->{num_triggered_monitors} = 0;
     return $self;
+}
+
+=head2 toggle_trigger
+
+Toggle whether a monitor wants to shutdown and, if enough agree, call shutdown
+
+=cut
+
+sub toggle_trigger {
+    my ( $self, $monitor_name, $toggle ) = @_;
+    my $logger = $self->{logger};
+
+    if ( !defined $toggle || $toggle !~ /^0|1$/ ) {
+        $logger->logdie( "Called with invalid value for toggle" );
+    }
+
+    # set/unset the toggle
+    if ( $toggle and not $self->{triggered_monitors}->{$monitor_name} ) {
+        $self->{triggered_monitors}->{$monitor_name} = 1;
+    } elsif ( $self->{triggered_monitors}->{$monitor_name} and not $toggle ) {
+        delete( $self->{triggered_monitors}->{$monitor_name} );
+    } else {
+
+        # seen it before, don't care
+        return;
+    }
+
+    # Store how many are triggered, and shutdown if limit reached
+    $self->{num_triggered_monitors} = scalar keys %{ $self->{triggered_monitors} };
+    $logger->debug( $self->{num_triggered_monitors} . " monitors are ready to shutdown" );
+    if ( $self->{num_triggered_monitors} >= $self->{params}->{shutdown_after_triggered_monitors} ) {
+        $self->shutdown();
+    }
+}
+
+=head2 shutdown
+
+Shutdown the system, if not in test mode
+
+=cut
+
+sub shutdown {
+    my $self   = shift;
+    my $logger = $self->{logger};
+
+    $logger->info( "Shutting down" );
+
+    if ( $self->{params}->{test} ) {
+        $logger->info( "Not really shutting down because running in test mode" );
+    } else {
+
+        # Do the actual shutdown
+        my @cmd = ( $self->{params}->{shutdown_binary} );
+
+        # have any args?
+        if ( $self->{params}->{shutdown_args} ) {
+            push @cmd, @{ $self->{params}->{shutdown_args} };
+        }
+
+        if ( $self->{params}->{use_sudo} ) {
+            unshift( @cmd, 'sudo' );
+        }
+        $logger->debug( "Shutting down with cmd: " . join( ' ', @cmd ) );
+
+        my ( $in, $out, $err );
+        if ( not IPC::Run::run( \@cmd, \$in, \$out, \$err, IPC::Run::timeout( 10 ) ) ) {
+            $logger->error( "Could not run '" . join( ' ', @cmd ) . "': $!" );
+        }
+        if ( $err ) {
+            $logger->error( "Could not shutdown: $err" );
+        }
+        if ( $self->{params}->{exit_after_trigger} ) {
+            exit;
+        }
+    }
 }
 
 =head2 start
@@ -275,8 +389,9 @@ sub new {
 Start the shutdown daemon
 
 =cut
+
 sub start {
-    my $self = shift;
+    my $self   = shift;
     my $logger = $self->{logger};
 
     $logger->info( "Started" );
@@ -284,32 +399,28 @@ sub start {
     $logger->info( "Sleeping $self->{params}->{sleep_before_run} seconds before starting monitoring" );
 
     sleep( $self->{params}->{sleep_before_run} );
-    
-    my $monitor = $self->{monitors}->{hdparm};
 
-    $monitor->run();
-    $logger->info( "Shutting down" );
-    if( $self->{test} ){
-	$logger->info( "Not really shutting down because running in test mode" );
-    }else{
-        # Do the actual shutdown
-        my @cmd = ( $self->{params}->{shutdown_binary}, '-h', 'now' );
-        if( $self->{params}->{use_sudo} ){
-            unshift( @cmd, 'sudo' );
-        }
-        my( $in, $out, $err );
-	if( ! IPC::Run::run( \@cmd, \$in, \$out, \$err, IPC::Run::timeout( 10 ) ) ) {
-	    $logger->error( "Could not run '" . join( ' ', @cmd ) . "': $!" );
-	}
-	if( $err ) {
-	    $logger->error( "Monitor hdparm could not shutdown: $err" );
-	}
-        if( $self->{params}->{exit_after_trigger} ){
-            exit;
-        }
+    # set up timers then wait forever
+    foreach my $monitor_name ( keys %{ $self->{monitors} } ) {
+        my $monitor = $self->{monitors}->{$monitor_name};
+
+        $logger->debug( "Setting timer for monitor $monitor_name: $monitor->{params}->{loop_sleep} seconds" );
+        $monitor->{timer} = AnyEvent->timer(
+            after    => 0,
+            interval => $monitor->{params}->{loop_sleep},
+            cb       => sub {
+                if ( $monitor->run() ) {
+                    $self->toggle_trigger( $monitor_name, 1 );
+                } else {
+                    $self->toggle_trigger( $monitor_name, 0 );
+                }
+            }
+        );
     }
-}
+    $logger->debug( 'Entering main listen loop using ' . $AnyEvent::MODEL );
+    AnyEvent::CondVar->recv;
 
+}
 
 =head1 AUTHOR
 
@@ -357,4 +468,4 @@ See http://dev.perl.org/licenses/ for more information.
 
 =cut
 
-1; # End of Daemon::Shutdown
+1;    # End of Daemon::Shutdown
--- lib/Daemon/Shutdown/Monitor/hdparm.pm
+++ lib/Daemon/Shutdown/Monitor/hdparm.pm
@@ -14,12 +14,11 @@ Daemon::Shutdown::Monitor::hdparm - a hdparm specific monitor
 
 =head1 VERSION
 
-Version 0.06
+Version 0.07
 
 =cut
 
-our $VERSION = '0.06';
-
+our $VERSION = '0.07';
 
 =head1 SYNOPSIS
 
@@ -81,34 +80,34 @@ monitor:
 =cut
 
 sub new {
-    my $class = shift;
+    my $class  = shift;
     my %params = @_;
-    
+
     # Validate the config file
     %params = validate_with(
         params => \%params,
         spec   => {
-            loop_sleep   => {
-		regex    => qr/^\d*$/,
-                default  => 60,
-	    },
+            loop_sleep => {
+                regex   => qr/^\d*$/,
+                default => 60,
+            },
             trigger_time => {
-                regex => qr/^\d*$/,
+                regex   => qr/^\d*$/,
                 default => 3600,
             },
-	    disks   => {
-		type   => ARRAYREF,
-                default => [ '/dev/sda' ],
-		callbacks => { 'Disks exist' => 
-		    sub{ 
-			my $disks_ref = shift();
-			foreach my $disk ( @{ $disks_ref } ){
-			    return 0 if ! -e $disk;
-			}
-			return 1;
-		    },
-		},
-	    },
+            disks => {
+                type      => ARRAYREF,
+                default   => ['/dev/sda'],
+                callbacks => {
+                    'Disks exist' => sub {
+                        my $disks_ref = shift();
+                        foreach my $disk ( @{$disks_ref} ) {
+                            return 0 if !-e $disk;
+                        }
+                        return 1;
+                    },
+                },
+            },
             use_sudo => {
                 default => 0,
                 regex   => qr/^[1|0]$/,
@@ -117,9 +116,9 @@ sub new {
     );
     my $self = {};
     $self->{params} = \%params;
-    
+
     $self->{trigger_pending} = 0;
-    
+
     bless $self, $class;
     my $logger = Log::Log4perl->get_logger();
     $self->{logger} = $logger;
@@ -133,64 +132,64 @@ sub new {
 Run the hdparm spindown Monitor
 
 =cut
+
 sub run {
     my $self = shift;
-    
+
     my $logger = $self->{logger};
 
     $logger->info( "Monitor started running: hdparm" );
-    
-    # The loop
-    while( 1 ){
-        my $conditions_met = 1;
 
-        # Test each disk
-        foreach my $disk( @{ $self->{params}->{disks} } ){
-            $logger->debug( "Monitor hdparm testing $disk" );
-	    my @cmd = ( qw/hdparm -C/, $disk );
-            if( $self->{params}->{use_sudo} ){
-                unshift( @cmd, 'sudo' );
-            }
-            $logger->debug( "Monitor hdparm CMD: " . join( ' ', @cmd ) );
-            my( $in, $out, $err );
-	    if( ! IPC::Run::run( \@cmd, \$in, \$out, \$err, IPC::Run::timeout( 10 ) ) ) {
-		$logger->warn( "Could not run '" . join( ' ', @cmd ) . "': $!" );
-	    }
-	    if( $err ) {
-		$logger->error( "Monitor hdparm: $err" );
-                $conditions_met = 0;
-	    }
-            
-            # If any of the disks are active, the conditions for trigger are not met
-            if( $out =~ m/drive state is:  active/s ){
-                $logger->debug( "Monitor hdparm sees disk is active: $disk" );
-                $conditions_met = 0;
-            }
+    my $conditions_met = 1;
+
+    # Test each disk
+    foreach my $disk ( @{ $self->{params}->{disks} } ) {
+        $logger->debug( "Monitor hdparm testing $disk" );
+        my @cmd = ( qw/hdparm -C/, $disk );
+        if ( $self->{params}->{use_sudo} ) {
+            unshift( @cmd, 'sudo' );
+        }
+        $logger->debug( "Monitor hdparm CMD: " . join( ' ', @cmd ) );
+        my ( $in, $out, $err );
+        if ( not IPC::Run::run( \@cmd, \$in, \$out, \$err, IPC::Run::timeout( 10 ) ) ) {
+            $logger->warn( "Could not run '" . join( ' ', @cmd ) . "': $!" );
+        }
+        if ( $err ) {
+            $logger->error( "Monitor hdparm: $err" );
+            $conditions_met = 0;
         }
 
-        if( $conditions_met ) {
-            # All disks are spun down! Set the trigger_pending time.
-            $self->{trigger_pending} = $self->{trigger_pending} || time();
-            if( $self->{trigger_pending} and 
-                ( time() - $self->{trigger_pending} ) >=  $self->{params}->{trigger_time} ){
-                # ... and the trigger was set, and time has run out: time to return!
-                $logger->info( "Monitor hdparm trigger time reached after $self->{params}->{trigger_time}" );
-                return 1;
-            }
-
-            $logger->info( "Monitor hdparm found all disks spun down: trigger pending." );
-       }else{
-            if( $self->{trigger_pending} ){
-                $logger->info( "Monitor hdparm trigger time being reset because of disk activity" );
-            }
-            # Conditions not met - reset the trigger incase it was previously set.
-            $self->{trigger_pending} = 0;
+        # If any of the disks are active, the conditions for trigger are not met
+        if ( $out =~ m/drive state is:  active/s ) {
+            $logger->debug( "Monitor hdparm sees disk is active: $disk" );
+            $conditions_met = 0;
         }
-        $logger->debug( "Monitor hdparm sleeping $self->{params}->{loop_sleep}" );
-        sleep( $self->{params}->{loop_sleep} );
     }
-}
 
+    if ( $conditions_met ) {
+
+        # All disks are spun down! Set the trigger_pending time.
+        $self->{trigger_pending} = $self->{trigger_pending} || time();
+        if ( $self->{trigger_pending}
+            and ( time() - $self->{trigger_pending} ) >= $self->{params}->{trigger_time} )
+        {
+
+            # ... and the trigger was set, and time has run out: time to return!
+            $logger->info( "Monitor hdparm trigger time reached after $self->{params}->{trigger_time}" );
+            return 1;
+        }
+
+        $logger->info( "Monitor hdparm found all disks spun down: trigger pending." );
+    } else {
+        if ( $self->{trigger_pending} ) {
+            $logger->info( "Monitor hdparm trigger time being reset because of disk activity" );
+        }
+
+        # Conditions not met - reset the trigger incase it was previously set.
+        $self->{trigger_pending} = 0;
+    }
+    return 0;
+}
 
 =head1 AUTHOR
 
@@ -208,4 +207,4 @@ See http://dev.perl.org/licenses/ for more information.
 
 =cut
 
-1; # End of Daemon::Shutdown::Monitor::hdparm
+1;    # End of Daemon::Shutdown::Monitor::hdparm
